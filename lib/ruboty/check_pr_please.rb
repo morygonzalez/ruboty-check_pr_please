68588ddf69b78e5b4568466108c6b71d7c12e7c0
森井ゴンザレス <morygonzalez@gmail.com>
2015-03-25 10:20:13 +0900

    require Necessary gems

--- lib/ruboty/check_pr_please.rb
+++ lib/ruboty/check_pr_please.rb
@@ -1,4 +1,6 @@
 require "ruboty"
+require "active_support/all"
+require "octokit"
 require "ruboty/check_pr_please/version"
 require "ruboty/actions/check_pr_please"
 require "ruboty/handlers/check_pr_please"
